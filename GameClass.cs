9032d5848dcdc270d2f21c833d4affe798b84072
Olga Didenko <dokokoster9@gmail.com>
2021-07-11 22:57:20 +0300

    Update GameClass.cs
    добавила коменты

--- GameClass.cs
+++ GameClass.cs
@@ -4,14 +4,22 @@ using System.Text;
 
 namespace terminal_rpg
 {
-    public class GameClass
+    public class GameClass // все игровые классы наследуются от GameClass
     {
-        static int HandAttack = 0; 
+        static int HandAttack = 0; // const вместо static, сила аттаки - 20
 
         public GameClass(int HandAttack)
         {
-             HandAttack = 1;
+             HandAttack = 1; // не нужно, проинициализируй филд
         }
+        
+        // protected Attack() {
+        // }
+        
+        // все классы переопределяют метод Attack
+        // @override
+        // public Attack() {}
+            
 
         GameClass Dudu = new GameClass (HandAttack);
     }
